bf1d362402f7f324a4b7b56d81da2683ff778f07
RadoslawZemlo <radoslawzemlo1@gmail.com>
2023-03-11 02:02:57 +0100

    use behavior subject to emit todos

--- src/app/core/services/todos/todos.service.ts
+++ src/app/core/services/todos/todos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from '@interfaces/todo.interface';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, take } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +9,17 @@ import { Observable } from 'rxjs';
 export class TodosService {
   private todosUrl = 'http://localhost:3000/todos';
 
+  private readonly sourceTodos$: BehaviorSubject<Todo[]> = new BehaviorSubject<
+    Todo[]
+  >([]);
+  todos$: Observable<Todo[]> = this.sourceTodos$.asObservable();
+
   constructor(private readonly http: HttpClient) {}
 
-  getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl);
+  getTodos(): void {
+    this.http
+      .get<Todo[]>(this.todosUrl)
+      .pipe(take(1))
+      .subscribe((todos) => this.sourceTodos$.next(todos));
   }
 }
--- src/app/modules/todos/components/todos/todos.component.ts
+++ src/app/modules/todos/components/todos/todos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodosService } from '@app/core/services/todos/todos.service';
 import { Todo } from '@interfaces/todo.interface';
 import { Observable } from 'rxjs';
@@ -8,8 +8,12 @@ import { Observable } from 'rxjs';
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.scss'],
 })
-export class TodosComponent {
-  todos: Observable<Todo[]> = this.todosService.getTodos();
+export class TodosComponent implements OnInit {
+  todos$: Observable<Todo[]> = this.todosService.todos$;
 
   constructor(private readonly todosService: TodosService) {}
+
+  ngOnInit(): void {
+    this.todosService.getTodos();
+  }
 }
