19729c3d92f2ecc46bc6cb520ae8b22f6b7c3e98
RadoslawZemlo <radoslawzemlo1@gmail.com>
2023-04-17 06:31:20 +0200

    Add tests for todos and todo component

--- src/app/modules/todos/components/todo/todo.component.spec.ts
+++ src/app/modules/todos/components/todo/todo.component.spec.ts
@@ -1,23 +1,88 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { SharedModule } from '@app/shared/shared.module';
 import { TodoComponent } from './todo.component';
+import { TodosService } from '@app/core/services/todos/todos.service';
+import { Todo } from '@app/core/interfaces/todo.interface';
 
 describe('TodoComponent', () => {
+  const todo: Todo = {
+    task: 'todo new',
+    priority: 2,
+    addedAt: new Date(),
+    completed: false,
+    id: 1,
+  };
+
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
+  let loader: HarnessLoader;
+
+  const todosServiceSpy = jasmine.createSpyObj<TodosService>('TodoService', [
+    'updateTodo',
+    'deleteTodo',
+  ]);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoComponent ]
-    })
-    .compileComponents();
+      declarations: [TodoComponent],
+      imports: [MatDialogModule, SharedModule],
+      providers: [{ provide: TodosService, useValue: todosServiceSpy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
+    component.todo = todo;
+
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 3 buttons', async () => {
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+
+    expect(buttons.length).toBe(3);
+  });
+
+  it('should call todoService.updateTodo', async () => {
+    const toggleTodo = { ...todo, completed: !todo.completed };
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+    const toggleButton = buttons[0];
+
+    await toggleButton.click();
+
+    expect(todosServiceSpy.updateTodo).toHaveBeenCalledOnceWith(
+      todo,
+      toggleTodo
+    );
+  });
+
+  it('should call editTodo method', async () => {
+    spyOn(component, 'editTodo');
+
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+    const toggleButton = buttons[1];
+
+    await toggleButton.click();
+
+    expect(component.editTodo).toHaveBeenCalled();
+  });
+
+  it('should call deleteTodo method', async () => {
+    spyOn(component, 'deleteTodo');
+
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+    const toggleButton = buttons[2];
+
+    await toggleButton.click();
+
+    expect(component.deleteTodo).toHaveBeenCalled();
   });
 });
--- src/app/modules/todos/components/todos/todos.component.spec.ts
+++ src/app/modules/todos/components/todos/todos.component.spec.ts
@@ -1,16 +1,70 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from '@app/shared/shared.module';
 import { TodosComponent } from './todos.component';
+import { TodosService } from '@app/core/services/todos/todos.service';
+import { Todo } from '@app/core/interfaces/todo.interface';
+import { of } from 'rxjs';
 
 describe('TodosComponent', () => {
+  const mockTodos: Todo[] = [
+    {
+      task: 'todo one',
+      priority: 2,
+      addedAt: new Date(),
+      completed: false,
+      id: 1,
+    },
+    {
+      task: 'todo two',
+      priority: 3,
+      addedAt: new Date(),
+      completed: false,
+      id: 2,
+    },
+    {
+      task: 'todo three',
+      priority: 1,
+      addedAt: new Date(),
+      completed: true,
+      id: 3,
+    },
+    {
+      task: 'todo four',
+      priority: 2,
+      addedAt: new Date(),
+      completed: false,
+      id: 4,
+    },
+  ];
+
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
 
+  const todosServiceSpy = jasmine.createSpyObj<TodosService>('TodoService', [
+    'todos$',
+    'getTodos',
+    'getTodo',
+    'addTodo',
+    'updateTodo',
+    'deleteTodo',
+  ]);
+
+  todosServiceSpy.todos$ = of(mockTodos);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodosComponent ]
-    })
-    .compileComponents();
+      declarations: [TodosComponent],
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+        SharedModule,
+      ],
+      providers: [{ provide: TodosService, useValue: todosServiceSpy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TodosComponent);
     component = fixture.componentInstance;
@@ -19,5 +73,14 @@ describe('TodosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should recive a list of tasks', (done) => {
+    component.todos$.subscribe((todos) => {
+      expect(todos.length).toBe(4);
+      expect(todos[0].task).toEqual('todo one');
+
+      done();
+    });
   });
 });
