33c5be30e9b7a34dbbf4f7cf59066b8090b35838
RadoslawZemlo <radoslawzemlo1@gmail.com>
2023-04-17 07:06:24 +0200

    Add tests for dashboard and header components

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,23 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatButtonHarness } from '@angular/material/button/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 3 buttons', async () => {
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+
+    expect(buttons.length).toBe(2);
   });
 });
--- src/app/modules/dashboard/components/dashboard/dashboard.component.spec.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.spec.ts
@@ -1,16 +1,58 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { SharedModule } from '@app/shared/shared.module';
+import { TodosService } from '@app/core/services/todos/todos.service';
 import { DashboardComponent } from './dashboard.component';
+import { Todo } from '@app/core/interfaces/todo.interface';
+import { of } from 'rxjs';
 
 describe('DashboardComponent', () => {
+  const mockTodos: Todo[] = [
+    {
+      task: 'todo one',
+      priority: 2,
+      addedAt: new Date(),
+      completed: false,
+      id: 1,
+    },
+    {
+      task: 'todo two',
+      priority: 3,
+      addedAt: new Date(),
+      completed: false,
+      id: 2,
+    },
+    {
+      task: 'todo three',
+      priority: 1,
+      addedAt: new Date(),
+      completed: true,
+      id: 3,
+    },
+    {
+      task: 'todo four',
+      priority: 2,
+      addedAt: new Date(),
+      completed: false,
+      id: 4,
+    },
+  ];
+
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+  const todosServiceSpy = jasmine.createSpyObj<TodosService>('TodoService', [
+    'todos$',
+    'getTodos',
+  ]);
+
+  todosServiceSpy.todos$ = of(mockTodos);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-    })
-    .compileComponents();
+      declarations: [DashboardComponent],
+      imports: [SharedModule],
+      providers: [{ provide: TodosService, useValue: todosServiceSpy }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
@@ -19,5 +61,14 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should recive a list of tasks', (done) => {
+    component.todos$.subscribe((todos) => {
+      expect(todos.length).toBe(4);
+      expect(todos[0].task).toEqual('todo one');
+
+      done();
+    });
   });
 });
